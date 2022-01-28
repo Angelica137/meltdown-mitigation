df7714550181efabeed84b819a5c7a7980e4e0ec
Angelica <Angelica137@users.noreply.github.com>
2022-01-28 22:30:42 +0000

    test is_critically_balanced_returns False product grater than 500000, green

--- tests/test_conditionals.py
+++ tests/test_conditionals.py
@@ -3,3 +3,7 @@ from scripts.conditionals import *
 
 def test_is_criticality_balanced_returns_true():
     is_criticality_balanced(750, 600) == True
+
+
+def test_is_criticality_balanced_returns_false_product_fail():
+    is_criticality_balanced(799, 700) == False
