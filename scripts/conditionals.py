b3c58781efaab8ba0babe8668757ee133973c900
Angelica <Angelica137@users.noreply.github.com>
2022-02-03 18:36:54 +0000

    remove comment

--- scripts/conditionals.py
+++ scripts/conditionals.py
@@ -30,5 +30,4 @@ def fail_safe(temp: int, neutrons_produced: int, threshold: int) -> str:
         return 'NORMAL'
     if criticality < threshold*0.9:  # O(n^2) + O(1)
         return 'LOW'
-    # if criticality > threshold*0.9:  # O(n^2) + O(1)
     return 'DANGER'
